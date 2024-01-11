7eff0fe877a1458fba6e6cd7f9998c9fd4c07664
tan1ty <tatiana.kovalenko1992@gmail.com>
2024-01-11 12:17:22 +0100

    Apply Functional Programming to Convert Strings to URL Slugs

---
+++ string-to-url.js
@@ -0,0 +1,9 @@
+function urlSlug(title) {
+  const newArr = title
+    .split(' ')
+    .map((word) => word.slice(0, 1).toLowerCase() + word.slice(1))
+    .filter((word) => word.trim() !== '')
+    .join('-');
+  return newArr;
+}
+urlSlug(' Winter Is  Coming');
