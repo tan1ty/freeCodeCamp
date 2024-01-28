21679ea6f1c98387fc90ce7f3bef989e7a5dba44
tan1ty <tatiana.kovalenko1992@gmail.com>
2024-01-28 17:36:35 +0100

    add new solutions and comments

--- src/functional/uniqueValue.js
+++ src/functional/uniqueValue.js
@@ -1,14 +1,38 @@
+import * as R from 'ramda';
+
+/*Diff Two Arrays
+Compare two arrays and return a new array with any items only found in one of the two given arrays,
+but not both. In other words, return the symmetric difference of the two arrays.*/
+
+//solution with Set for big data
+
 function diffArray(arr1, arr2) {
-  const set1 = new Set(arr1);
-  const set2 = new Set(arr2);
+  const setOne = new Set(arr1);
+  const setTwo = new Set(arr2);
 
-  const difference1 = [...set1].filter((x) => !set2.has(x));
-  const difference2 = [...set2].filter((x) => !set1.has(x));
+  const diffOne = [...setOne].filter((x) => !setTwo.has(x));
+  const diffTwo = [...setTwo].filter((x) => !setOne.has(x));
 
-  return [...difference1, ...difference2];
+  return [...diffOne, ...diffTwo];
 }
 
 diffArray(
   ['diorite', 'andesite', 'grass', 'dirt', 'pink wool', 'dead shrub'],
+  ['diorite', 'andesite', 'grass', 'dirt', 'dead shrub']
+);
+
+//simple solution
+
+function diffArr(arr1, arr2) {
+  const newArr = arr1.concat(arr2);
+  return newArr.filter((item) => !arr1.includes(item) || !arr2.includes(item));
+}
+
+diffArr([1, 4, 6, 8, 9], [4, 7, 5, 8]);
+
+//with Ramda library
+
+const diff = R.difference(
+  ['andesite', 'grass', 'dirt', 'pink wool', 'dead shrub'],
   ['diorite', 'andesite', 'grass', 'dirt', 'dead shrub']
 );
